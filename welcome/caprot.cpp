c0e159738e8eeaf3e446fc2093c20265dd83c54f
Baige He <hebaige@169-231-125-150.wireless.ucsb.edu>
2023-04-11 17:57:14 -0700

    adding Makefile and cpp file

---
+++ welcome/caprot.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    cout << "here" << endl;
+    int num, newnum;
+    string strN;
+
+    
+    num  = atoi(argv[argc - 1]);
+    cout << num << endl;
+    
+    getline(cin, strN);
+    
+    string k;
+    k = strN;
+    long strNLength = k.length();
+    
+    if(num > strNLength){
+        num = num%strNLength;
+    }
+    
+    if(num > strNLength){
+        num = num%strNLength;
+    }
+    
+    for(int i = 0; i < strNLength; ++i){
+        if(isupper(k[i])){
+            k[i] = tolower(k[i]);
+            
+        }
+    }
+    if(num > 0){
+        for(int i = 0; i < strNLength - num; ++i){
+            cout << "in for loop" << endl;
+            if(isupper(strN[i])){
+                k[i + num] = toupper(k[i + num]);
+            }
+        }
+        
+        for(int i = k.length() - num; i < strNLength; ++i){
+            cout << "entered other for loop" << endl;
+            if(isupper(strN[i])){
+                k[i + num - k.length()] = toupper(k[i + num - k.length()]);
+            }
+        }
+        
+       
+    }
+    else if(num < 0){
+        
+        newnum = -num;
+        for(int i = (strNLength - 1); i >= newnum; --i){
+            if(isupper(strN[i])){
+                k[i - newnum] = toupper(k[i - newnum]);
+            }
+        }
+        for(int i = 0; i < newnum; ++i){
+            if(isupper(strN[i])){
+                k[i + strNLength - newnum] = toupper(k[i + strNLength - newnum]);
+            }
+        }
+        
+    }
+    
+    cout << k << endl;
+    return 0;
+}
+
