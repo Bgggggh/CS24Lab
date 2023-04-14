5cf55fec1b8133a2a4428b1310db2c8bcab4a1b7
Baige He <hebaige@169-231-125-85.wireless.ucsb.edu>
2023-04-14 11:43:43 -0700

    lab2

--- fibvec/FibVec.cpp
+++ fibvec/FibVec.cpp
@@ -3,6 +3,57 @@
 // This provides exception types:
 #include <stdexcept>
 
+FibVec::FibVec(){
+    fib_vector = new int[1];
+}
+
+FibVec::~FibVec(){
+    delete[] fib_vector;
+}
+
+size_t FibVec::capacity() const
+{
+    return size;
+}
+
+size_t FibVec::count() const
+{
+    return counts;
+}
+
+size_t FibVec::lookup(size_t index) const
+{
+    if(index < counts){
+        return fib_vector[index];
+    }
+    throw std::out_of_range("out of range");
+}
+
+void FibVec::insert(int value, size_t index)
+{
+    if(index < counts){
+        fib_vector[index] = value;
+    }
+    throw std::out_of_range("out of range");
+}
+
+int FibVec::pop()
+{
+    if(counts != 0){
+        
+    }
+}
+
+void FibVec::push(int value)
+{
+    
+}
+
+size_t FibVec::remove(size_t index)
+{
+    
+}
+
 
 FibVec* create_fibvec() {
   // Used by the autograder to make a new FibVec.
--- fibvec/FibVec.h
+++ fibvec/FibVec.h
@@ -6,14 +6,24 @@
 
 class FibVec {
   // Member Variables
-
+private:
+    size_t size;
+    size_t counts;
+    int* fib_vector;
   // Helper Functions
 
 public:
-  // Constructor and Destructor
-
+  // Constructor and Destructoe
+    FibVec();
+    ~FibVec();
   // Member Functions
-
+    size_t capacity() const;
+    size_t count() const;
+    void insert(int value, size_t index);
+    size_t lookup(size_t index) const;
+    int pop();
+    void push(int value);
+    size_t remove(size_t index);
 };
 
 // Used by the autograder:
