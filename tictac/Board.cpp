4278f191e69c5749cbcdaaf534069c9820432145
Bgggggh <122944781+Bgggggh@users.noreply.github.com>
2023-04-25 11:21:14 -0700

    Add files via upload

--- tictac/Board.cpp
+++ tictac/Board.cpp
@@ -126,9 +126,7 @@ char Board::check_win(){
 
 bool Board::check_draw(){
     //check it when arr.size() == 9
-    return ((check_win() != 'X') && (check_win() != 'O'))
-
-
+    return ((check_win() != 'X') && (check_win() != 'O'));
 }
 
 void Board::print_result(const Move& move) {
