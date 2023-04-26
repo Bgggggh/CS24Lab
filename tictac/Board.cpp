2e39430cc77e71e7e5c4ec691ef7e2b346ad88cf
Bgggggh <122944781+Bgggggh@users.noreply.github.com>
2023-04-25 21:14:34 -0700

    Add files via upload

--- tictac/Board.cpp
+++ tictac/Board.cpp
@@ -45,8 +45,8 @@ bool Board::check_over(){
 }
 
 char Board::check_order(const Move& move){
-    if((n + 1) != move.number){
-        cout << n <<' ' << move.number << endl;
+    if(n != move.number){
+        //cout << n <<' ' << move.number << endl;
         throw InvalidMove("the number order is incorrect");
         //check the number order: 1, 2, 3, 4, 5.....
     }
