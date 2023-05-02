a3b45b760fbddcd752409eb520eadd48bfd30afe
Bgggggh <122944781+Bgggggh@users.noreply.github.com>
2023-05-02 09:20:34 -0700

    Add files via upload

--- setree/Node.cpp
+++ setree/Node.cpp
@@ -1,1 +1,54 @@
 #include "Node.h"
+Node::Node() : val('-'), left(nullptr), right(nullptr), count(0) {}
+Node::Node(const string& val) : val(val), left(nullptr), right(nullptr), count(1) {}
+Node::Node(const Node& other) : val(other.val), left(nullptr), right(nullptr), count(other.count){
+    if(other.left){
+        left = new Node(*other.left);
+    }
+    else if(other.right){
+        right = new Node(*other.right);
+    }
+}
+
+Node::Node(Node&& other) : val(move(other.val)), left(other.left), right(other.left), count(other.count){
+    other.left = nullptr;
+    other.right = nullptr;
+    other.count = 0;
+}
+
+Node::~Node(){
+    delete left;
+    delete right;
+}
+
+Node& Node::operator = (const Node& other){
+    if(this != &other){
+        val = other.val;
+        count = other.count;
+        delete left;
+        left = nullptr;
+        delete right;
+        right = nullptr;
+        if(other.left){
+            left = new Node(*other.left);
+        }
+        else if(other.right){
+            right = new Node(*other.right);
+        }
+    }
+    return *this;
+}
+
+Node& Node::operator=(Node&& other) {
+    if (this != &other) {
+        val = std::move(other.val);
+        count = other.count;
+        delete left;
+        left = other.left;
+        other.left = nullptr;
+        delete right;
+        right = other.right;
+        other.right = nullptr;
+        other.count = 0;
+    }
+    return *this;}
--- setree/Node.h
+++ setree/Node.h
@@ -1,7 +1,27 @@
 #ifndef NODE_H
 #define NODE_H
+#include <string>
+using namespace std;
 
 // Use this file to declare your Node type.
 // Implement Node member functions and helper functions in Node.cpp.
+class Node{
+public:
+    Node();
+    Node(const string& value);
+    Node(const Node& other);
+    Node(Node&& other);
+    ~Node();
+   
+    Node& operator = (const Node& other);
+    Node& operator = (Node&& other);
+
+    Node* left;
+    Node* right;
+    int count;
+    string val;
+};
+
+
 
 #endif
