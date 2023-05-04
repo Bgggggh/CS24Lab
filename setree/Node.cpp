11b8925a1eb218df0e2f3056a7a31fdc2d3a77eb
Bgggggh <122944781+Bgggggh@users.noreply.github.com>
2023-05-04 15:11:52 -0700

    Add files via upload

--- setree/Node.cpp
+++ setree/Node.cpp
@@ -22,10 +22,10 @@ void Node::copyF(Node* node, Node* &insertNode){
 }
 
 Node::~Node(){
-    delete left;
+    /*delete left;
     left = NULL;
     delete right;
-    right = NULL;
+    right = NULL;*/
 }
 
 size_t Node::clearF(Node* node) const{
@@ -33,6 +33,8 @@ size_t Node::clearF(Node* node) const{
         return 0;
     }
     size_t count1 = countF(node);
+    clear(node->left);
+    clear(node->right);
     delete node;
     node = nullptr;
     return count1;
