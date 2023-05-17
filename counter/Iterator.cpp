a4dbb77adfe5621c676de83357d02f5465d95b0e
Bgggggh <122944781+Bgggggh@users.noreply.github.com>
2023-05-17 07:53:00 -0700

    Add files via upload

--- counter/Iterator.cpp
+++ counter/Iterator.cpp
@@ -11,9 +11,7 @@ int Counter::Iterator::value() const {
 }
 
 Counter::Iterator& Counter::Iterator::operator ++ () {
-    if(curr_node){
-        curr_node = curr_node->next;
-    }
+    curr_node = curr_node->next;
     return *this;
 }
 
