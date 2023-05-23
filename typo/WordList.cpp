b9cbf0582508f43608e9600ac017a28c17a45e54
Bgggggh <122944781+Bgggggh@users.noreply.github.com>
2023-05-22 22:58:22 -0700

    Add files via upload

--- typo/WordList.cpp
+++ typo/WordList.cpp
@@ -27,16 +27,16 @@ Heap WordList::correct(const std::vector<Point>& points, size_t maxcount, float 
         if (word.length() == points.size()) {
             float totalScore = 0.0f;
             for (size_t i =0;i < points.size();++i){
-
-                float dx = points[i].x - QWERTY[word[i]-'a'].x;
-                float dy = points[i].y - QWERTY[word[i]-'a'].y;
-                float distance = std::sqrt(dx * dx + dy * dy);
+                float dx = points[i].x-QWERTY[word[i]-'a'].x;
+                float dy = points[i].y-QWERTY[word[i]-'a'].y;
+                float distance = dx * dx + dy * dy;
                 float score = 1.0f / (10 * distance * distance + 1);
 
                 totalScore += score;
             }
             float score = totalScore / static_cast<float>(points.size());
-            if (score >= cutoff) {
+
+            if (score >= cutoff && score > 0 && score < 1) {
                 result.push(word, score);
             }
         }
